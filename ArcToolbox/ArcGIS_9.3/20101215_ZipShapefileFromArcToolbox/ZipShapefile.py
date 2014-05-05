b3c0127dcd4718715b304ec81a920831a7939e6d
mrantala <mjrantal@umn.edu>
2014-05-05 13:28:51 -0500

    Updated the code from 12/15/2010 to allow a user to skip the .lock files. If .lock files exist, the code will error out. This option prevents that, however, should be used with caution because if your shapefile is being used, it could be corrupted. USE AT YOUR OWN RISK.

--- ArcToolbox/ArcGIS_9.3/20101215_ZipShapefileFromArcToolbox/ZipShapefile.py
+++ ArcToolbox/ArcGIS_9.3/20101215_ZipShapefileFromArcToolbox/ZipShapefile.py
@@ -6,9 +6,13 @@
 #
 # It is set up to be used as an ArcGIS
 #
-# Peace
-#
 # 12/10/2010
+#
+#
+# Updated 5/5/2014 with the option to ignore .lock files. This can be a bit
+# dangerous because if the shapefile is being editted, unexpected results
+#(including corrupting your shapefile) may occur.
+#
 # Matthew Rantala
 # MapRantala@Gmail.com
 # https://MapRantala.com
@@ -20,9 +24,10 @@ import os
 import glob
 
 theShapeFile = sys.argv[1]
-wellsZipFile = sys.argv[2]
+outputZipFile = sys.argv[2]
+skipLockFile = sys.argv[3]
 
-def zipShapefile(inShapefile, newZipFN):
+def zipShapefile(inShapefile, newZipFN, skipLockFile):
     print 'Starting to Zip '+inShapefile+' to '+newZipFN
 
     if not (os.path.exists(inShapefile)):
@@ -41,11 +46,12 @@ def zipShapefile(inShapefile, newZipFN):
 
     for infile in glob.glob( inShapefile.lower().replace(".shp",".*")):
         print infile
-        zipobj.write(infile,os.path.basename(infile),zipfile.ZIP_DEFLATED)
+        if not ((os.path.splitext(infile.lower())[1] == ".lock") and (skipLockFile.lower() == "true")):
+            zipobj.write(infile,os.path.basename(infile),zipfile.ZIP_DEFLATED)
 
     zipobj.close()
 
     return True
 
-zipShapefile(theShapeFile,wellsZipFile)
+zipShapefile(theShapeFile,outputZipFile,skipLockFile)
 print "done!"
